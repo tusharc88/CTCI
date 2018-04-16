321a459d4c2fdb20b909f9597a1d51f28eb0eda7
tusharc88 <tushar54983@gmail.com>
2018-04-15 21:37:57 -0700

    Adding RemoveDups for LinkedLists

---
+++ LinkedLists/Node.js
@@ -0,0 +1,6 @@
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+modules.export = Node;
---
+++ LinkedLists/RemoveDups.js
@@ -0,0 +1,22 @@
+let Node = require('./Node');
+
+// using buffer
+function removeDups(head) {
+  if (head === null) return null;
+  let prev = head;
+  let current = this.head.next;
+  const map = {};
+  map[prev.data] = true;
+
+  while (current !== null) {
+    if (!map[current.data]) {
+      map[current.data] = true;
+    } else {
+      prev.next = current.next;
+    }
+    prev = current;
+    current = current.next;
+  }
+
+  return head;
+}
